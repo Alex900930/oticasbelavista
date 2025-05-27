6daa89b0be6cd6d221f9332de7ab2eb2a6f8c959
aherreramilet <aherrera@dl.cl>
2025-05-27 17:14:16 -0300

    agregado favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Site Óptica Bela Vista',
   description: 'Óptica Bela Vista em Canindé, Ceará: Armações a partir de R$39,90, Exame de Vista Grátis e Brinde Exclusivo! Encontre a armação perfeita para você e cuide da sua visão com qualidade e economia. Agende seu exame hoje!',
+ icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
